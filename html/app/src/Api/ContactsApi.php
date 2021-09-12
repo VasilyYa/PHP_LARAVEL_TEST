3eb4941ffe3d53ba4218e89dd24459ad2edb8bf8
VasilyYa <v.yakovina@gmail.com>
2021-09-13 01:47:19 +0500

    master|validation of email/phone using symfony validator component is done

--- html/app/src/Api/ContactsApi.php
+++ html/app/src/Api/ContactsApi.php
@@ -3,11 +3,16 @@
 namespace App\Api;
 
 use App\Controllers\ContactsController;
+use Symfony\Component\Validator\Constraints\Email as EmailConstraint;
+use Symfony\Component\Validator\Constraints\Regex as RegexConstraint;
+use Symfony\Component\Validator\Validation;
 
 
 class ContactsApi extends Api
 {
+
     public string $apiName = 'contacts';
+
 
     protected function indexAction()
     {
@@ -21,18 +26,29 @@ class ContactsApi extends Api
 
     protected function createAction()
     {
-        //validate POST params:
+
         $email = $this->requestParams['email'] ?? null;
         $phone = $this->requestParams['phone'] ?? null;
         $message = $this->requestParams['message'] ?? null;
-        if ($email === null) {
+
+
+        //validate post-params:
+        $validator = Validation::createValidator();
+        $emailConstraint = new EmailConstraint();
+        $phoneConstraint = new RegexConstraint('/^\+?[0-9\(\)\-\s]{5,20}$/');
+        //email
+        $errors = $validator->validate($email,$emailConstraint);
+        if (0 !== count($errors)) {
             return $this->response(array('result' => 'Failed', 'error' => 'Email is not valid'), 400);
         }
-        if ($phone === null) {
+        //phone
+        $errors = $validator->validate($phone,$phoneConstraint);
+        if (0 !== count($errors)) {
             return $this->response(array('result' => 'Failed', 'error' => 'Phone is not valid'), 400);
         }
 
-        //write to Database and make a response to client:
+
+        //write to Database:
         if (ContactsController::createContactOrReturnNull($email, $phone, $message)) {
 
             return $this->response(array('result' => 'Ok', 'notice' => 'Data added to database'), 200);
